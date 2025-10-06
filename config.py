01b5b0eb79d7507f5ed57e5626c39459896c835e
saikatsingh <mailmetosaikat676@gmail.com>
2025-10-06 15:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "29563132")
     API_HASH = environ.get("API_HASH", "b39be032fc0c567d0cda60dbea99606e")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://workwithsaikat:saikat9735@cluster0.0e5vp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
